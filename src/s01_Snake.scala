c4d50cccf21d054b0fb6f146e1e1b7f92f4afb8f
josejuan <dev@computer-mind.com>
2012-05-24 10:13:58 +0200

    learning...

--- src/s01_Snake.scala
+++ src/s01_Snake.scala
@@ -1,4 +1,5 @@
 // Arrow left/right to move snake!
+
 import java.awt.Color
 import java.awt.event.{ActionEvent, ActionListener}
 import scala.Array.canBuildFrom
@@ -7,18 +8,12 @@ import scala.swing.event.{Key, KeyPressed}
 import javax.swing.Timer
 
 object s01_Snake extends SimpleSwingApplication {
-	val S = 11
-	// ground has SxS cells
-	val W = 10
-	// cell has WxW pixels
-	val D = Array(1, 0, 0, -1, -1, 0, 0, 1)
-	// move types: right, up, left, down
-	var d = 0
-	// actual move type
-	var s = Array((S >> 1, S >> 1))
-	// snake body FILO
-	val rnd = new java.util.Random()
-	// I can't remember...
+	val S = 11 // ground has SxS cells
+	val W = 10 // cell has WxW pixels
+	val D = Array(1, 0, 0, -1, -1, 0, 0, 1) // move types: right, up, left, down
+	var d = 0 // actual move type
+	var s = Array((S >> 1, S >> 1)) // snake body FILO
+	val rnd = new java.util.Random() // I can't remember...
 	var f = food // actual food pos
 
 	// return a free cell (to put food)
@@ -47,6 +42,7 @@ object s01_Snake extends SimpleSwingApplication {
 					g.drawString("Ouch!", 10, 10)
 				}
 			}
+
 			listenTo(keys)
 			reactions += {
 				case KeyPressed(_, Key.Left, _, _) => d = (d + 1) % 4
